6eb8d609f95fe0b0f6a57daf6c8ca0ffad6063e4
Maduma <maduma@pt.lu>
2015-06-09 11:08:48 +0000

    box display

---
+++ angular-piggybank.js
@@ -0,0 +1,40 @@
+/* global angular */
+(function() {
+'use strict';
+
+angular
+    .module('maPiggybank', ['ngMaterial'])
+    .directive('maPiggybank', piggybank);
+    
+function piggybank() {
+
+    var directive = {
+        restrict: 'E',
+        templateUrl: 'angular-piggybank.template.html',
+        scope: {},
+        bindToController: {
+            rw: "=",
+            box: "="
+        },
+        controller: ctrl,
+        controllerAs : 'ctrl'
+    };
+    
+    return directive;
+    
+    function ctrl() {
+        var self = this;
+        self.inc = function() {
+            if (self.rw) {
+                self.box.amount++;
+            } 
+        };
+        self.dec = function() {
+            if (self.rw) {
+                self.box.amount--;
+            }
+        };
+    }
+}
+
+})();
